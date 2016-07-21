efdf33f1fd57fca05d4161ec1135e52974b8b48c
cs4jcmac <cs4jcmac@cs4jcmac-imac.corp.ha.org.hk>
2016-07-21 18:34:48 +0800

    Initial Commit

---
+++ aboutUs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  aboutUs
+//
+//  Created by cs4jcmac on 21/7/2016.
+//  Copyright © 2016 jc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ aboutUs/SecondViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SecondViewController.swift
+//  aboutUs
+//
+//  Created by cs4jcmac on 21/7/2016.
+//  Copyright © 2016 jc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+//, UITableViewDelegate, UITableViewDataSource {
+//    @IBOutlet weak var mytable: UITableView!
+    
+    var car = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"]
+    var carName = ["Lamborghini", "Drift", "Ferrari", "Hyundai","Mercedes Benz","Mitsubishi","Nissan","Volkswagen"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        mytable.delegate = self
+//        mytable.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+//        return car.count
+//    }
+    //tableview delegate
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+//    {
+//        return 2
+//    }
+
+    
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:
+//        NSIndexPath) -> UITableViewCell {
+//        let cellIdentifier = "Cell"
+//        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier,
+//                                                               forIndexPath: indexPath)
+//        // Configure the cell...
+//        cell.textLabel?.text = car[indexPath.row]
+//        return cell
+//    }
+    
+    
+    
+    
+    
+    
+
+}
